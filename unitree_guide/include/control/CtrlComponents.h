8fe0abc22db5c3ddd14175ca8bf0026523df90d3
Tipriest <a1503741059@163.com>
2025-10-06 16:15:50 +0800

    chore

--- unitree_guide/include/control/CtrlComponents.h
+++ unitree_guide/include/control/CtrlComponents.h
@@ -4,96 +4,87 @@
 #ifndef CTRLCOMPONENTS_H
 #define CTRLCOMPONENTS_H
 
+#include "Gait/WaveGenerator.h"
+#include "common/unitreeRobot.h"
+#include "control/BalanceCtrl.h"
+#include "control/Estimator.h"
+#include "interface/CmdPanel.h"
+#include "interface/IOInterface.h"
 #include "message/LowlevelCmd.h"
 #include "message/LowlevelState.h"
-#include "interface/IOInterface.h"
-#include "interface/CmdPanel.h"
-#include "common/unitreeRobot.h"
-#include "Gait/WaveGenerator.h"
-#include "control/Estimator.h"
-#include "control/BalanceCtrl.h"
-#include <string>
 #include <iostream>
+#include <string>
 
 #ifdef COMPILE_DEBUG
 #include "common/PyPlot.h"
-#endif  // COMPILE_DEBUG
+#endif // COMPILE_DEBUG
 
-struct CtrlComponents{
+struct CtrlComponents {
 public:
-    CtrlComponents(IOInterface *ioInter):ioInter(ioInter){
-        lowCmd = new LowlevelCmd();
-        lowState = new LowlevelState();
-        contact = new VecInt4;
-        phase = new Vec4;
-        *contact = VecInt4(0, 0, 0, 0);
-        *phase = Vec4(0.5, 0.5, 0.5, 0.5);
-    }
-    ~CtrlComponents(){
-        delete lowCmd;
-        delete lowState;
-        delete ioInter;
-        delete robotModel;
-        delete waveGen;
-        delete estimator;
-        delete balCtrl;
+  CtrlComponents(IOInterface *ioInter) : ioInter(ioInter) {
+    lowCmd = new LowlevelCmd();
+    lowState = new LowlevelState();
+    contact = new VecInt4;
+    phase = new Vec4;
+    *contact = VecInt4(0, 0, 0, 0);
+    *phase = Vec4(0.5, 0.5, 0.5, 0.5);
+  }
+  ~CtrlComponents() {
+    delete lowCmd;
+    delete lowState;
+    delete ioInter;
+    delete robotModel;
+    delete waveGen;
+    delete estimator;
+    delete balCtrl;
 #ifdef COMPILE_DEBUG
-        delete plot;
-#endif  // COMPILE_DEBUG
-    }
-    LowlevelCmd *lowCmd;
-    LowlevelState *lowState;
-    IOInterface *ioInter;
-    QuadrupedRobot *robotModel;
-    WaveGenerator *waveGen;
-    Estimator *estimator;
-    BalanceCtrl *balCtrl;
-
-#ifdef COMPILE_DEBUG
-    PyPlot *plot;
-#endif  // COMPILE_DEBUG
-
-    VecInt4 *contact;
-    Vec4 *phase;
-
-    double dt;
-    bool *running;
-    CtrlPlatform ctrlPlatform;
-
-    void sendRecv(){
-        ioInter->sendRecv(lowCmd, lowState);
-    }
-
-    void runWaveGen(){
-        waveGen->calcContactPhase(*phase, *contact, _waveStatus);
-    }
-
-    void setAllStance(){
-        _waveStatus = WaveStatus::STANCE_ALL;
-    }
-
-    void setAllSwing(){
-        _waveStatus = WaveStatus::SWING_ALL;
-    }
-
-    void setStartWave(){
-        _waveStatus = WaveStatus::WAVE_ALL;
-    }
-
-    void geneObj(){
-        estimator = new Estimator(robotModel, lowState, contact, phase, dt);
-        balCtrl = new BalanceCtrl(robotModel);
+    delete plot;
+#endif // COMPILE_DEBUG
+  }
+  LowlevelCmd *lowCmd;
+  LowlevelState *lowState;
+  IOInterface *ioInter;
+  QuadrupedRobot *robotModel;
+  WaveGenerator *waveGen;
+  Estimator *estimator;
+  BalanceCtrl *balCtrl;
 
 #ifdef COMPILE_DEBUG
-        plot = new PyPlot();
-        balCtrl->setPyPlot(plot);
-        estimator->setPyPlot(plot);
-#endif  // COMPILE_DEBUG
-    }
+  PyPlot *plot;
+#endif // COMPILE_DEBUG
+
+  VecInt4 *contact;
+  Vec4 *phase;
+
+  double dt;
+  bool *running;
+  CtrlPlatform ctrlPlatform;
+
+  void sendRecv() { ioInter->sendRecv(lowCmd, lowState); }
+
+  void runWaveGen() {
+    waveGen->calcContactPhase(*phase, *contact, _waveStatus);
+  }
+
+  void setAllStance() { _waveStatus = WaveStatus::STANCE_ALL; }
+
+  void setAllSwing() { _waveStatus = WaveStatus::SWING_ALL; }
+
+  void setStartWave() { _waveStatus = WaveStatus::WAVE_ALL; }
+
+  void geneObj() {
+    estimator = new Estimator(robotModel, lowState, contact, phase, dt);
+    balCtrl = new BalanceCtrl(robotModel);
+
+#ifdef COMPILE_DEBUG
+    plot = new PyPlot();
+    balCtrl->setPyPlot(plot);
+    estimator->setPyPlot(plot);
+#endif // COMPILE_DEBUG
+  }
 
 private:
-    WaveStatus _waveStatus = WaveStatus::SWING_ALL;
-
+  WaveStatus _waveStatus = WaveStatus::SWING_ALL;
 };
 
-#endif  // CTRLCOMPONENTS_H
+#endif // CTRLCOMPONENTS_H
